0457d280496444c450a48a3bb93bc62452918575
MobileMars76874 <72148112+MobileMars76874@users.noreply.github.com>
2021-02-21 15:33:39 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+mouseEvent = "";
+
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+
+
+canvas.addEventListener("mousedown", mymousedown);
+function mymousedown(e){
+mouseEvent = "mousedown";
+color = document.getElementById("C").value;
+line_width = document.getElementById("W").value;
+radius = document.getElementById("R").value;
+}
+
+canvas.addEventListener("mouseup", mymouseup);
+function mymouseup(e){
+mouseEvent = "mouseup";
+}
+
+canvas.addEventListener("mouseleave", mymouseleave);
+function mymouseleave(e){
+mouseEvent = "mouseleave";
+}
+
+canvas.addEventListener("mousemove", mymousemove);
+function mymousemove(e){
+
+current_x = e.clientX - canvas.offsetLeft;
+current_y = e.clientY - canvas.offsetTop;
+
+if(mouseEvent = "mousedown"){
+ctx.beginPath()
+ctx.strokeStyle = color;
+ctx.lineWidth = line_width;
+ctx.arc(current_x, current_y, radius, 0 ,2 * Math.PI);
+ctx.stroke()
+}
+}
+
